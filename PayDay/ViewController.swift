f1c1118bb95692f8b4b997fc232a95f93e603913
Oleksandr Burla <Alex@oleksandrs-macbook-air.local>
2016-03-15 09:44:48 +0200

    Initial Commit

---
+++ PayDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PayDay
+//
+//  Created by Oleksandr Burla on 3/15/16.
+//  Copyright © 2016 Oleksandr Burla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
